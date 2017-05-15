8de38bcab875a0867dee27d29862c018379020bc
Anna Flynn <anna.flynn@mlb.com>
2017-05-15 14:06:09 -0400

    add image resources & TODOs for homework

--- js/findyourperfectcollege.js
+++ js/findyourperfectcollege.js
@@ -1,26 +1,37 @@
-var submitQuiz = function() {
+var submitMajorQuiz = function () {
 
-  var gpa = document.getElementsByName("gpa")[0].value;
-  console.log("gpa is: " + gpa);
 
-  var majorList = document.getElementsByName("major");
-  for (var i = 0; i < majorList.length; i++){
-    var possibleMajor = majorList[i];
-    if (possibleMajor.checked){
-      console.log("one major is: " + possibleMajor.value);
+};
+
+var submitFoodQuiz = function () {
+    // TODO : think about what this function should do!
+    // model it after the submitEasyQuiz function, reading in the values you select on the quiz page
+};
+
+
+var submitEasyQuiz = function () {
+
+    var gpa = document.getElementsByName( "gpa" )[ 0 ].value;
+    console.log( "gpa is: " + gpa );
+
+    var majorList = document.getElementsByName( "major" );
+    for ( var i = 0; i < majorList.length; i++ ) {
+        var possibleMajor = majorList[ i ];
+        if ( possibleMajor.checked ) {
+            console.log( "one major is: " + possibleMajor.value );
+        }
     }
-  }
 
-  var regionList = document.getElementsByName("region");
-  for (var i = 0; i < regionList.length; i++){
-    var possibleRegion = regionList[i];
-    if (possibleRegion.checked){
-      console.log("region is: " + possibleRegion.value);
-      break;
+    var regionList = document.getElementsByName( "region" );
+    for ( var i = 0; i < regionList.length; i++ ) {
+        var possibleRegion = regionList[ i ];
+        if ( possibleRegion.checked ) {
+            console.log( "region is: " + possibleRegion.value );
+            break;
+        }
     }
-  }
 
-  var tuition = document.getElementsByName("tuition")[0].value;
-  console.log("tuition is: " + tuition);
+    var tuition = document.getElementsByName( "tuition" )[ 0 ].value;
+    console.log( "tuition is: " + tuition );
 
-  };
+};
---
+++ js/searchcolleges.js
@@ -0,0 +1,2 @@
+// TODO : define the search function here!
+// check out findyourperfectcollege.js for help in how to define a function!
