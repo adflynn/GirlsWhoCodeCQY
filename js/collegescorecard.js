a703b82344041bdfbe4c020a0a08e3058d35bbd4
Jess Wetstone <jessica.wetstone@oliverwyman.com>
2017-05-19 20:22:06 -0400

    additional field changes

--- js/collegescorecard.js
+++ js/collegescorecard.js
@@ -1,6 +1,6 @@
 var queryCollegeScorecardApi = function(data){
   $.extend(data,{ api_key : 'jh4T7Ei9av4SziJYbiOoSk77dzRB67m9zUX0mE3P'});
-  $.extend(data,{ _fields : 'id,school.name,school.city,school.state,2014.student.size,2014.admissions.admission_rate.overall' });
+  $.extend(data,{ _fields : 'id,school.name,school.city,school.state,school.school_url,2014.admissions.admission_rate.overall' });
   return $.ajax({
     url: 'https://api.data.gov/ed/collegescorecard/v1/schools',
     type: 'GET',
@@ -10,10 +10,16 @@ var queryCollegeScorecardApi = function(data){
   });
 };
 
-var constructCollegeScorecardSearch = function(name, min_admission_rate, max_admission_rate){
+var constructCollegeScorecardSearch = function(name, city, state, min_admission_rate, max_admission_rate){
   var queryFilters = {};
   if(name){
     $.extend(queryFilters,{'school.name': name});
+  }
+  if(city){
+    $.extend(queryfilters,{'school.city': city});
+  }
+  if(state){
+    $.extend(queryFilters,{'school.state': state});
   }
   if(min_admission_rate && max_admission_rate){
     $.extend(queryFilters,{'2014.admissions.admission_rate.overall__range': min_admission_rate + '..' + max_admission_rate});
@@ -21,7 +27,7 @@ var constructCollegeScorecardSearch = function(name, min_admission_rate, max_adm
   return queryFilters;
 };
 
-queryCollegeScorecardApi(constructCollegeScorecardSearch(null,'0.20','0.40')).done(function(data){
+queryCollegeScorecardApi(constructCollegeScorecardSearch(null,null,null,'0.20','0.40')).done(function(data){
   var schools = [];
   $.each(data.results, function(i, val){
     schools.push(val);
